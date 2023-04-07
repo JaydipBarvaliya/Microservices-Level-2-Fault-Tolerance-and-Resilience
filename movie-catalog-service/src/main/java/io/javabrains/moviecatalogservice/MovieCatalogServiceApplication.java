e06f12457d468be5a1ce1ddeeadd741d833ddfce
Jaydip Barvaliya <jaydipbarvaliya55@gmail.com>
2023-04-06 21:04:45 -0400

    .idea added.

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,5 +1,6 @@
 package io.javabrains.moviecatalogservice;
 
+import io.javabrains.moviecatalogservice.Controller.CatalogController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
